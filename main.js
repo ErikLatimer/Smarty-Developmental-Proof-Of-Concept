66b35a9609a94bb90771f60a03c10dee21c1aef5
ErikLatimer <eriklatimer99@Gmail.com>
2018-08-07 11:36:22 -0400

    I installed a little more than just webpack XD. I actually installed semantic-ui-react, semantic-ui, I got jquery working and I installed webpack and babel for the JSX translation. Everything, including react, semantic, and electron now work seamlessly in a working environment. There was a trick though to get Jquery to be recognized by electron, something about window.modules and such. It was a line of actually script I had to add before, and after all the module imports. Weird. And I still have the minifed react #200 error within the electron window, but its not effecting development, so I'll make sure to deal with that later. Now on to the meat and potatoes of the main window now I guess. I've already begun work on it with semantic, so I'll be picking that up where I left off. Oh, I removed html-webpack-plugin. Found it pointless, my index.hmtl is just going to point to the ouput folder and file for now. Sorry this is basically a duplicate XD. Forgot to add the modified files, I only added the new files in the last one.

--- main.js
+++ main.js
@@ -6,3 +6,5 @@ function createWindow() {
 }
 
 Electron.app.on('ready',createWindow);
+
+//I eventually want a login window for this. But for right now, I'm just gonna focus on the main screen after login. That is what the current index.html is going to focus on.
--- react_renderer.js
+++ react_renderer.js
@@ -1,3 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+import { Container, Button } from 'semantic-ui-react';
 
+var greetings = (
+    <div> 
+    </div>
+);
+
+ReactDOM.render(
+    greetings,
+    document.getElementById('entry')
+);
