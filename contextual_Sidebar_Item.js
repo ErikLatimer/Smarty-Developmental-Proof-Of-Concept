a6e81d4a77c33695b9d02a6999000b8da3b5287c
Erik Latimer <eriklatimer99@Gmail.com>
2019-03-05 18:44:03 -0500

    The newly reorganized project structure.

--- contextual_Sidebar_Item.js
+++
@@ -1,8 +0,0 @@
-import { pane_map } from './render_Pane_Map';
-export class sidebar_Item {
-    constructor ( item_name, render_module ) {
-        this.name = item_name;
-        this.pane = render_module;
-    }
-    static render_map = pane_map;
-}
--- render_Pane_Map.js
+++
@@ -1,8 +0,0 @@
-import { eur_usd_pane } from './render_panes/eur_usd_pane';
-
-
-export var pane_map = {
-    Account : eur_usd_pane,
-         
-
-};
